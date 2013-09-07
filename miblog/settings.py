3703f2bcd2e1198dcb0705fdcee69a050cc7ce32
Eli J. Chavez <skat0@hotmail.com>
2013-09-07 12:53:38 -0430

    Cambio nombre BD

--- miblog/settings.py
+++ miblog/settings.py
@@ -17,7 +17,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'miblog',                      # Or path to database file if using sqlite3.
+        'NAME': 'pyblog',                      # Or path to database file if using sqlite3.
         'USER': 'root',                        # Not used with sqlite3.
         'PASSWORD': 'root',                    # Not used with sqlite3.
         'HOST': '127.0.0.1',                   # Set to empty string for localhost. Not used with sqlite3.
